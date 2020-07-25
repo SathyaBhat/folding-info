32733f4cda96c109965c2ef2957564c0a2fe8162
Sathyajith <sathya@sathyasays.com>
2020-07-26 01:40:17 +0300

    add version

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,7 @@
 from folding_stats import get_and_publish_stats
+VERSION="2.0.0"
 
 def lambda_handler(event, context):
+    print(f"Starting F@H stats lambda version {VERSION}")
     get_and_publish_stats()
+    print(f"Goodbye!")
